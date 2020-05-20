a8d8a60c9e8d6b012a246b1e0a7bf0851a9ce3fa
RafaGar <rafaelgarciacadenas@gmail.com>
2020-05-19 21:27:57 -0500

    Estructura del proyecto

---
+++ src/components/footer/footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-light bg-dark text-white">
+
+                <p>
+                    <h6>Teléfono de contacto: 52-4705-5660</h6>
+                </p>
+                <p>
+                    <h6>Correo de contacto: contacto@clinicapp@gmail.com</h6>
+                </p>
+                <p>
+                    <h6>Teléfono de contacto</h6>
+                </p>
+
+
+            </nav>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/components/landing/landing.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import doctor from '../../img/doctor.PNG'
+
+const Landing = () => {
+    return (
+
+        <div>
+            <div className="container">
+                <div className="card bg-dark text-black">
+                    <img src={doctor} className="card-img" />
+                    <div className="card-img-overlay">
+                    </div>
+                </div>
+            </div>
+
+
+            <div className="card container">
+                <div className="card-header text-center">
+                    <a name="quienes">
+                        <p className="">
+                            <h1>Nuestra Plataforma</h1>
+                        </p>
+                    </a>
+                </div>
+
+                <div className="card-body">
+                    <blockquote className="blockquote mb-0">
+                        <p>Clinicapp es una plataforma que permite a los profesionales de la salud acceder a una base de datos donde podrá conocer el Historial clínico de sus pacientes.</p>
+                    </blockquote>
+
+                    <div className="card-deck">
+                        <div className="card">
+                            <div className="card-body text-center">
+                                <p>
+                                    <h1>1.-</h1>
+                                </p>
+                                <p className="card-text">Da de alta un paciente en la plataforma o encuentra su expediente clínico en nuestra plataforma.</p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-body text-center">
+                                <p>
+                                    <h1>2.-</h1>
+                                </p>
+                                <p className="card-text">Elabora una nota médica con la información de la consulta.</p>
+                            </div>
+                        </div>
+                        <div className="card">
+                            <div className="card-body text-center">
+                                <p>
+                                    <h1>3.-</h1>
+                                </p>
+                                <p className="card-text">Guarda la información del paciente en la plataforma para que pueda ser accesible por otros especialistas.</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Landing
