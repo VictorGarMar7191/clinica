2cf87dd7544d8ecf30f6af1a2aeaefe44793786e
Sinhue Camacho <camachohorus@gmail.com>
2020-05-21 19:35:24 -0500

    tarjetas de paciente

---
+++ src/components/ConElements/ConElements.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const = () => {
+    return (
+        <div className="card">
+           <div className="d-flex justify"> 
+
+           </div>
+            
+        </div>
+    )
+}
+
+export default
